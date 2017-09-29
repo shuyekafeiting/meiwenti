4d6365863d392806b83e38c0cf92d63d8d3910dd
Ares <814940657@qq.com>
2017-09-29 11:49:55 +0800

    用git管理煤问题

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 // 应用入口文件
 
-// 检测PHP环境
+// 检测PHP环境1110
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 define('_PHP_FILE_',$_SERVER['SCRIPT_NAME']);
